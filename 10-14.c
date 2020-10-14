999c0797b80229edbdf1a813bda70c83e86af221
se-cancle <1664815002@qq.com>
2020-10-14 20:45:57 +0800

    程序编译

---
+++ 10-14.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+//#define DOUBLE(x) ((x)+(x))
+//#define SIZE 5
+//#define SIZE2  5;
+ int main(){
+	//printf("%s %d\n", __FILE__, __LINE__);
+	//表示程序编译日期和时间
+	//printf("%s %s\n", __DATE__, __TIME__);
+	//printf("%d\n", __STDC__);
+
+	//printf("%d\n", DOUBLE(10)*DOUBLE(10));
+
+	 //int a = SIZE;
+	 //int b = SIZE2;
+	 //int arr1[SIZE];
+	 //int arr2[SIZE2];//执行代码编译失败=>int arr2[5;];
+
+#if 1
+	 printf("hehe\n");
+#else
+	 printf("haha");
+	system("pause");
+	
+	return 0;
+}
